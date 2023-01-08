94017e02772084f9c8ea5fd974332c107dd262a0
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-01-08 10:12:16 +0700

    SiGG-FinancialTech-Service-Gateway-Quater-Program

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
+
+namespace GM.Service.Gateway
+{
+  
+   public class Program 
+   {
+      public static void Main(string[] args)   
+      {
+          var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();    
+          try
+          {
+            logger.Debug("init main");   
+            CreateWebHostBuilder(args).Build().Run();  
+          }  
+          catch (Exception ex)    
+          {
+              //NLog: catch setup errors   
+              logger.Error(ex, "Stopped programs because of exception");   
+              throw;  
+          }
+          finally  
+          {
+              // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)    
+              NLog.LogManager.Shutdown();    
+          }
+      }  
+      public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>   
+            WebHost.CreateDefaultBuilder(args)    
+                  .UseStartup<Startup>()
+                  .ConfigureLogging(logging =>  
+                  {
+                      logging.ClearProviders();   
+                      logging.SetMinimumLevel(LogLevel.Trace);   
+                  })  
+                  .UseNLog();   // NLog: setup NLog for Dependency injection  
+      }
+}
